504740e0ea0c753e4950cb0d72d8dc71d3c27158
ASHISH <officialwork.ashish@gmail.com>
2022-11-23 13:40:58 +0530

    FINCTIONS ADDED

---
+++ functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/* A VOID FUNCTION IS CREATED HERE */
+void print() {
+
+    std::cout << "ITS ASHISH KUMAR SINGH THIS SIDE " << endl;
+};
+
+int main () {
+
+    print() ; /* CALLING A VOID FUNCTION NAMED  PRINT */
+    print(); /* we can call any functon multiple number of times*/
+    return 0;
+
+
+
+
+    
+}
